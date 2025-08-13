7c1e3d5d767770645897ff8bf8b36a0aecc68ee6
naosudo <nao@nicoriwa.com>
2025-08-14 01:39:33 +0900

    Deviseの導入とtable作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_13_162406) do
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "firstname_kanji", null: false
+    t.string "lastname_kanji", null: false
+    t.string "firstname_katakana", null: false
+    t.string "lastname_katakana", null: false
+    t.date "birth_day", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
