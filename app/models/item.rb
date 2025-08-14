955e1b5e37a5f1b38e12557a696c35b37beb3e8e
naosudo <nao@nicoriwa.com>
2025-08-15 01:06:36 +0900

    商品出品機能を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,15 +9,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-
   validates :name, :image, :price, :description, :category_id, :status_id, :payment_id, :prefecture_id, :day_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  with_options numericality: { other_than: 0 } do
-    validates :category_id
-    validates :status_id
-    validates :payment_id
-    validates :prefecture_id
-    validates :day_id
-  end
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :status_id,   numericality: { other_than: 0, message: "Sales status can't be blank" }
+  validates :payment_id,  numericality: { other_than: 0, message: "Shipping fee status can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "Prefecture can't be blank" }
+  validates :day_id,      numericality: { other_than: 0, message: "Scheduled delivery can't be blank" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :create]
   
   root to: "items#index"
   
